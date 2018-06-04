a91d1ae3d99abbe6c9c9613e8e9a603f163f33b6
Jiaru Zhang <tianfeng.@sjtu.edu.cn>
2018-06-04 18:01:35 +0800

    deep_model

--- code/model/simple_deep_model/deep_model.py
+++ code/model/simple_deep_model/deep_model.py
@@ -1,86 +1,138 @@
 import tensorflow as tf
-import code.feature_engineering
 import numpy as np
+import os
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from code.feature_engineering import get_data
+
+data, label = get_data(data_path="../../../dataset/RNA_trainset/")
+
+rnas = []
+for rna in data:
+    encoder = {'A': 0, 'G': 1, 'C': 2, 'T': 3}
+    rna = list(map(lambda x: encoder[x], rna))
+    rnas.append(rna)
+enc = OneHotEncoder()
+rnas = enc.fit_transform(rnas).toarray()
+X_train, X_test, y_train, y_test = train_test_split(rnas, label, test_size=0.2, random_state=42)
+trainset = list(zip(X_train, y_train))
+testset = list(zip(X_test, y_test))
+print("Load dataset finished!")
+
+class Simple_Deep:
+    def __init__(self, path, para, trainset, testset):
+        self.graph = tf.Graph()
+        self.prediction, self.trainstep, self.loss = None, None, None
+        self._path = path
+        self.para = para
+        self.trainset = trainset
+        self.testset = testset
+        self.predict_threshold = None
+        with self.graph.as_default():
+            self._define_inputs()
+            self._build_graph()
+            self.initializer = tf.global_variables_initializer()
+            self.saver = tf.train.Saver()
+        self._initialize_session()
+
+    @property
+    def save_path(self):
+        if self._save_path is None:
+            save_path = '%s/checkpoint' % self._path
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+            save_path = os.path.join(save_path, 'model.ckpt')
+            self._save_path = save_path
+        return self._save_path
+
+    @property
+    def logs_path(self):
+        if self._logs_path is None:
+            logs_path = '%s/logs' % self._path
+            if not os.path.exists(logs_path):
+                os.makedirs(logs_path)
+            self._logs_path = logs_path
+        return self._logs_path
+
+    def _initialize_session(self):
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(graph=self.graph, config=config)
+
+        self.sess.run(self.initializer)
+
+    def _define_inputs(self):
+        self.input = tf.placeholder(
+            tf.float32,
+            shape=[None, self.para['dim']]
+        )
+        self.labels = tf.placeholder(
+            tf.float32,
+            shape=[None, self.para['label_dim']]
+        )
+        self.mask = tf.placeholder(
+            tf.float32,
+            shape=[None, self.para['label_dim']]
+        )
+        self.keep_prob = tf.placeholder(tf.float32, shape=[], name='keep_prob')
+        self.predict_threshold = tf.placeholder(tf.float32, shape=[], name='threshold')
+
+    def _build_graph(self):
+        batchsize = tf.shape(self.input)[0]
+        x = tf.reshape(self.input, [batchsize, self.para['len'], 4])
+        # x = tf.transpose(x, [0, 2, 1])
+        # filter = tf.Variable(tf.random_normal([tf.shape(x)[1], 4, 1, 1]))
+        conv = tf.layers.conv1d(x, 16, kernel_size=4, activation=tf.nn.relu)
+        out = tf.layers.max_pooling1d(conv, 3, strides=3)
+        cell_fw = tf.contrib.rnn.BasicLSTMCell(self.para['hidden_size'])
+        cell_bw = tf.contrib.rnn.BasicLSTMCell(self.para['hidden_size'])
+        output = tf.concat(tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, out, dtype=tf.float32)[0], 2)
+        len = int(output.shape[1]) - 1
+        output = tf.slice(output, [0, len, 0], [-1, 1, -1])
+        output = tf.reshape(output, [-1, 2 * self.para['hidden_size']])
+        output = tf.layers.dense(output, self.para['label_dim'])
+        self.prediction = tf.nn.sigmoid(output)
+        loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.labels, logits=output)
+        loss = tf.reduce_sum(tf.multiply(loss, self.mask)) / tf.reduce_sum(self.mask)
+        optimizer = tf.train.AdamOptimizer(self.para['lr'])
+        self.trainstep = optimizer.minimize(loss)
+        self.loss = loss
+
+        digit_prediction = tf.sign(self.prediction - self.predict_threshold)
+        weights = tf.sign(self.labels + 1)
+        self.test_accuracy = tf.metrics.accuracy(labels=self.labels, predictions=digit_prediction, weights=weights)
 
 
-# a = tf.Variable([1, 2, 3])
-# b = tf.Variable([1, 2, 5])
-# weight = tf.Variable([1, 1, 0])
-# loss = tf.losses.mean_squared_error(a, b, weights=weight)
-# sess = tf.Session()
-# sess.run(tf.global_variables_initializer())
-# print(sess.run(loss))
-class Model:
-    def __init__(self):
-        self.input = None
-        self.conv_output = None
-        self.output = None
-
-        self.sess = tf.Session()
-
-    def convs(self):
-        with tf.name_scope('conv_layers') as scope:
-            with tf.name_scope("conv1") as scope:
-                kernel = tf.Variable(tf.truncated_normal([4, 1, 1], dtype=tf.float32, stddev=1e-1), name='weights',
-                                     trainable=True)
-                conv = tf.nn.conv1d(self.input, kernel, 1, padding="SAME")
-                self.conv1 = tf.nn.relu(conv, name=scope)
-            with tf.name_scope("pool1") as scope:
-                self.pool1 = tf.layers.max_pooling1d(self.conv1, pool_size=2, strides=2)
-
-        self.conv_output = self.pool1
-
-    def denses(self):
-        with tf.name_scope('fc_layers') as scope:
-            # fc1
-            with tf.name_scope('fc1') as scope:
-                shape = int(np.prod(self.conv_output.get_shape()[1:]))
-                fc1w = tf.Variable(tf.truncated_normal([shape, 37],
-                                                       dtype=tf.float32,
-                                                       stddev=1e-1), name='weights')
-                fc1b = tf.Variable(tf.constant(0.1, shape=[37], dtype=tf.float32),
-                                   trainable=True, name='biases')
-                pool5_flat = tf.reshape(self.conv_output, [-1, shape])
-                fc1l = tf.nn.bias_add(tf.matmul(pool5_flat, fc1w), fc1b)
-                self.fc1 = tf.nn.sigmoid(fc1l)
-                # self.parameters += [fc1w, fc1b]
-
-        self.output = self.fc1
-
-    def build(self):
-        self.input = tf.placeholder(tf.float32, shape=[None, 300, 1])
-        # self.output = tf.placeholder(tf.int64, shape=[None, 37])
-        self.expected_output = tf.placeholder(tf.int64, shape=[None, 37])
-        self.weights = tf.placeholder(tf.int64, shape=[None, 37])
-        self.convs()
-        self.denses()
-
-        print(self.output.shape)
-        self.losses = tf.losses.mean_squared_error(self.expected_output, self.output, weights=self.weights)
-
-        self.train_step = tf.train.AdamOptimizer(6e-5).minimize(self.losses)
-
-    def train(self):
-        all_rna, all_label = code.feature_engineering.get_data_sep(data_path="../../../dataset/RNA_trainset/")
-        all_rna = np.array(all_rna)
-        all_label = np.array(all_label)
-        all_label = np.reshape(all_label, [])
-        X_train, X_test, y_train, y_test = train_test_split(all_rna, all_label, test_size=0.2, random_state=42)
-        print(y_train.shape)
-        print(y_train[0])
-
-        print(y_train[0].shape)
-        print(y_train)
-        weights = np.sign(y_train + 1)
-
-        self.build()
-        self.sess.run(tf.global_variables_initializer())
-        for step in range(3):
-            loss = self.sess.run(fetches=[self.losses], feed_dict={self.input: X_train, self.expected_output: y_train,
-                                                                   self.weights: weights})
-            print(loss)
+    def train(self, batch_size, epoch):
+        batch_per_epoch = int(len(self.trainset) / batch_size)
+        start_position = 0
+        for e in range(epoch):
+            for b in range(batch_per_epoch):
+                x, y = zip(*trainset[start_position: start_position + batch_size])
+                start_position += batch_size
+                y = np.array(y)
+                mask = y != -1
+                mask = mask.astype(np.float32)
+                feed_dict = {
+                    self.input: x,
+                    self.labels: y,
+                    self.mask: mask,
+                    self.keep_prob: 0.5
+                }
+                fetch = [self.trainstep, self.loss, self.prediction]
+                _, loss, pred = self.sess.run(fetch, feed_dict)
+                print("Train epoch {0} batch {1} loss {2}".format(e, b, loss))
+            x, y = zip(*testset)
+            feed_dict = {
+                self.input: x,
+                self.labels: y,
+                self.keep_prob: 1
+            }
+            accuracy = self.sess.run(fetches=[self.test_accuracy], feed_dict=feed_dict)
+            print("Test epoch {0} accuracy {1}".format(e, accuracy))
 
 
-m = Model()
-m.train()
+if __name__ == '__main__':
+    para = {'len': 300, 'label_dim': 37, 'dim': 1200, 'hidden_size': 256, 'lr': 1e-4}
+    model = Simple_Deep('./model', para, trainset, testset)
+    model.train(batch_size=100, epoch=5)
