a6b56d31b82441d2660cd235e512b325f814a50a
arthur <arthurfyc@gmail.com>
2018-06-05 19:43:30 +0800

    add attention

--- code/model/simple_deep_model/deep_model.py
+++ code/model/simple_deep_model/deep_model.py
@@ -141,6 +141,10 @@ class Simple_Deep:
         self.prediction = tf.nn.sigmoid(output)
         loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.labels, logits=output)
         loss = tf.reduce_sum(tf.multiply(loss, self.mask)) / tf.reduce_sum(self.mask)
+        weights = tf.trainable_variables()
+        l1_reg = tf.contrib.layers.l1_regularizer(scale=0.005)
+        regularization_penalty = tf.contrib.layers.apply_regularization(l1_reg, weights)
+        loss += regularization_penalty
         optimizer = tf.train.AdamOptimizer(self.para['lr'])
         self.trainstep = optimizer.minimize(loss)
         self.loss = loss
